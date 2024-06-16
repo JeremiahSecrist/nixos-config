d4ed770397e8bdc75b818d7ca28cd9229fd767bf
Jermeiah S <owner@arouzing.xyz>
2024-06-16 16:38:29 -0400

    update gnome config

--- modules/nixos/desktops/gnome/default.nix
+++ modules/nixos/desktops/gnome/default.nix
@@ -39,22 +39,26 @@ in
 
     # exclude the following packages from the default installation
     environment.gnome.excludePackages =
-      (with pkgs; [ gnome-photos gnome-tour ])
+      (with pkgs; [
+        gnome-photos
+        gnome-tour
+        gedit # text editor
+      ])
       ++ (with pkgs.gnome; [
         cheese # webcam tool
         gnome-music
-        #   gnome-terminal
-        gedit # text editor
+        gnome-terminal
         epiphany # web browser
         geary # email reader
         evince # document viewer
         gnome-characters
-        #totem # video player
+        totem # video player
         tali # poker game
         iagno # go game
         hitori # sudoku game
         atomix # puzzle game
       ]);
+
     programs = {
       xwayland.enable = lib.mkDefault true;
     };
