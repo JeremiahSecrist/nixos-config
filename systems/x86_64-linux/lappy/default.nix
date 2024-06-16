a98fc9f44d0a517ba1d7621cb087fb49093386da
Jermeiah S <owner@arouzing.xyz>
2024-06-16 16:49:55 -0400

    the great migration!

--- systems/x86_64-linux/lappy/default.nix
+++ systems/x86_64-linux/lappy/default.nix
@@ -24,25 +24,13 @@
   config
 , ...
 }:
-let
-  module = "";
-in
 {
-  # disabledModules = lib.mkForce [ "${modulePath}/nixos/modules/services/databases/etcd.nix" ];
-  # assertions = [
-  #   {
-  #     assertion = (builtins.elem "services/misc/etcd.nix" self.disabledModules);
-  #     message = "Can't disable etcd.nix";
-  #   }
-  # ];
-  # imports = [
-  #   (import ../../modules/nixos/disks/impermanence.nix {
-  #     device = "/dev/nvme0n1";
-  #     swapSize = "8G";
-  #   })
-  #   ./wiregaurd.nix
-  # ];
   internal = {
+    roles = {
+      gaming.enable = true;
+
+    };
+    desktop.gnome.enable = true;
     networking.asWireguard.enable = true;
     disko.impermanence = {
       enable = true;
@@ -68,98 +56,16 @@ in
   };
   virtualisation.docker.enable = true;
   users.users.sky.extraGroups = [ "docker" ];
-  systemd.services.bctl = {
-    enable = config.internal.disko.impermanence.enable;
-    wantedBy = [ "multi-user.target" ];
-    after = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''${pkgs.brightnessctl}/bin/brightnessctl set 10%'';
-    };
-  };
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
-  };
 
-  system.nixos.tags = [ "zramSwap" ];
 
-  zramSwap.enable = lib.mkIf (builtins.elem "zramSwap" config.system.nixos.tags) true;
-  # services.teamviewer.enable = true;
-  # services.rustdesk.enable = true;
-  system.activationScripts.diff = {
-    supportsDryActivation = true;
-    text = ''
-      ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff \
-           /run/current-system "$systemConfig"
-    '';
-  };
   environment.systemPackages = with pkgs; [
-    parsec-bin
     inputs.agenix.packages.x86_64-linux.default
     age-plugin-yubikey
-    mangohud
   ];
-  boot.kernel.sysctl = {
-    "kernel.sysrq" = 1;
-    "kernel.perf_event_paranoid" = 1;
-  };
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  # environment.sessionVariables = {
-  #   MESA_VK_DEVICE_SELECT_FORCE_DEFAULT_DEVICE = "${card}";
-  #   MESA_VK_WSI_PRESENT_MODE = "immediate";
-  #   KWIN_DRM_DEVICES = "/dev/dri/card${card}";
-  #   KMSDEVICE = "/dev/dri/card${card}";
-  # };
-  # # services.ollama = {
-  # #   enable = true;
-  # #   acceleration = "rocm";
-  # #   environmentVariables = {
-  # #     HSA_OVERRIDE_GFX_VERSION="10.3.0";
-  # #   };
-  # # };
-  #  services.udev.extraRules = ''
-  #       # ensure all cards don't get seat and master-of-seat tags
-  #       SUBSYSTEM=="drm", KERNEL=="card1", TAG-="seat", TAG-="master-of-seat", ENV{ID_FOR_SEAT}="", ENV{ID_PATH}=""
-  #       # adds seat, master-of-seat and mutter-device-preferred-primary tag for desired card
-  #       # TODO: are there any other tags here used by other compositors?
-  #       SUBSYSTEM=="drm", KERNEL=="card${card}", TAG+="seat", TAG+="master-of-seat", TAG+="mutter-device-preferred-primary"
-  #     '';
-  # Enable the X11 windowing system.
+
   services.xserver.enable = true;
   services.switcherooControl.enable = lib.mkIf (builtins.elem "hello" config.system.nixos.tags) true;
-  # Enable the Budgie Desktop environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.desktopManager.plasma6.enable = true;
-  environment.gnome.excludePackages =
-    (with pkgs; [
-      gnome-photos
-      gnome-tour
-      gedit # text editor
-    ])
-    ++ (with pkgs.gnome; [
-      cheese # webcam tool
-      gnome-music
-      gnome-terminal
-      epiphany # web browser
-      geary # email reader
-      evince # document viewer
-      gnome-characters
-      totem # video player
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-    ]);
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
-  programs.gamescope.enable = true;
   # services.xserver.displayManager.defaultSession = "plasma";
   programs.command-not-found.enable = false;
   programs.nix-index-database.comma.enable = true;
@@ -175,14 +81,7 @@ in
   hardware.enableAllFirmware = true;
 
   services = {
-    # miniupnpd = {
-    #   enable = true;
-    #   externalInterface = [
-    #     "eth0"
-    #     "wlan0"
-    #   ];
-    #   natpmp = true;
-    # };
+
     flatpak.enable = true;
     preload.enable = true;
   };
@@ -203,10 +102,7 @@ in
     #     # ];
     # };
     # };
-    gamemode = {
-      enable = true;
-      enableRenice = true;
-    };
+
   };
   networking = {
     # dhcpcd.enable = false;
@@ -224,12 +120,7 @@ in
     };
   };
 
-  nix = {
-    registry = {
-      self.flake = inputs.self;
-      nixpkgs.flake = inputs.nixpkgs;
-    };
-  };
+
 
   system.stateVersion = "23.11";
 }
